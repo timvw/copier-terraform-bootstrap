a799df6fa94599e1a8ab9d8fce810e80643c8492
Tim Van Wassenhove <github@timvw.be>
2026-01-09 13:30:11 +0100

    feat: add project_source tag

--- template/terraform/modules/tags/main.tf
+++ template/terraform/modules/tags/main.tf
@@ -2,6 +2,7 @@ locals {
   tags = {
     "be.icteam:project"             = "{{ project_name }}"
     "be.icteam:iac_tool"            = "terraform"
+    "be.icteam:iac_source"          = "{{ project_source }}"
     "be.icteam:terraform:workspace" = terraform.workspace
     "be.icteam:terraform:stack"     = var.stack
     "be.icteam:environment"         = var.environment
